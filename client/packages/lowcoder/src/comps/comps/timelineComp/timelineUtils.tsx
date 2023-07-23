5b4c60d807222e64dfeeccaab324e6463f2da6aa
mou <10402885@qq.com>
2023-07-23 22:27:38 +0800

    Add Timeline Component: 1. Supports multiple display modes 2. Supports setting titles, subtitles, labels, icons, icon colors, and all text of color 3.Support dynamic display of unfinished nodes 4. Supports reverse display of nodes 5. Support click event, expose clickedItem and clickedIndex properties

---
+++ client/packages/lowcoder/src/comps/comps/timelineComp/timelineUtils.tsx
@@ -0,0 +1,22 @@
+import { BoolCodeControl, jsonControl } from "comps/controls/codeControl";
+import { check } from "util/convertUtils";
+import {timelineNode} from './timelineConstants'
+
+export function convertTimeLineData(data: any) {
+  return data === "" ? [] : checkDataNodes(data) ?? [];
+}
+
+function checkDataNodes(value: any, key?: string): timelineNode[] | undefined {
+  return check(value, ["array", "undefined"], key, (node, k) => {
+    check(node, ["object"], k);
+    check(node["title"], ["string"], "title");
+    check(node["subTitle"], ["string", "undefined"], "subTitle");
+    check(node["label"], ["string", "undefined"], "label");
+    check(node["color"], ["string", "undefined"], "color");
+    check(node["titleColor"], ["string", "undefined"], "titleColor");
+    check(node["subTitleColor"], ["string", "undefined"], "subTitleColor");
+    check(node["lableColor"], ["string", "undefined"], "lableColor");
+    check(node["dot"], ["string", "undefined"], "dot");
+    return node;
+  });
+}
