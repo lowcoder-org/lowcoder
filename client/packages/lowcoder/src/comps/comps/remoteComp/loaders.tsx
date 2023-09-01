44a7c5e5e5d4b27a3f98f66f749fbf5ab5adea5a
FalkWolsky <fw@falkwolsky.com>
2023-09-01 15:03:01 +0200

    fix:Enabling RemoteCops to load external Source, update SDK Version

--- client/packages/lowcoder/src/comps/comps/remoteComp/loaders.tsx
+++ client/packages/lowcoder/src/comps/comps/remoteComp/loaders.tsx
@@ -18,7 +18,7 @@ async function npmLoader(remoteInfo: RemoteCompInfo): Promise<CompConstructor | 
 async function bundleLoader(remoteInfo: RemoteCompInfo): Promise<CompConstructor | null> {
   const { packageName, packageVersion = "latest", compName } = remoteInfo;
   const entry = `/${packageName}/${packageVersion}/index.js?v=${REACT_APP_COMMIT_ID}`;
-  const module = await import(/* @vite-ignore */ entry);
+  const module = await import(/* webpackIgnore: true */ entry);
   const comp = module.default?.[compName];
   if (!comp) {
     throw new Error(trans("npm.compNotFound", { compName }));
