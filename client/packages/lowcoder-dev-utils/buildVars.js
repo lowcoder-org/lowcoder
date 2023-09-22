4de487c0646c96e075265397034cfd6845296de9
RAHEEL <mraheeliftikhar1994@gmail.com>
2023-09-22 23:18:33 +0500

    feat: added env variable for custom welcome message on org auth page

--- client/packages/lowcoder-dev-utils/buildVars.js
+++ client/packages/lowcoder-dev-utils/buildVars.js
@@ -36,6 +36,10 @@ export const buildVars = [
     defaultValue: "",
   },
   {
+    name: "LOWCODER_SHOW_CUSTOM_AUTH_WELCOME_TEXT",
+    defaultValue: "",
+  },
+  {
     name: "REACT_APP_ENV",
     defaultValue: "production",
   },
