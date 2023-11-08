ea9d6fcb5d88717ade08e5632661792d9327203a
Abdul Qadir <abdul.qadir@ikhwatech.com>
2023-11-09 01:10:56 +0500

    Add handling for api delays in case of rate limit

--- server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/framework/plugin/SharedPluginServices.java
+++ server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/framework/plugin/SharedPluginServices.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.function.Consumer;
 
 import org.lowcoder.api.framework.plugin.endpoint.PluginEndpointHandler;
+import org.lowcoder.infra.config.repository.ServerConfigRepository;
 import org.lowcoder.plugin.api.LowcoderServices;
 import org.lowcoder.plugin.api.PluginEndpoint;
 import org.lowcoder.plugin.api.event.LowcoderEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +20,9 @@ import lombok.RequiredArgsConstructor;
 public class SharedPluginServices implements LowcoderServices
 {
 	private final PluginEndpointHandler pluginEndpointHandler;
+
+	@Autowired
+	private ServerConfigRepository serverConfigRepository;
 	
 	private List<Consumer<LowcoderEvent>> eventListeners = new LinkedList<>();
 
@@ -40,5 +45,15 @@ public class SharedPluginServices implements LowcoderServices
 	public void registerEndpoints(String urlPrefix, List<PluginEndpoint> endpoints) 
 	{
 		pluginEndpointHandler.registerEndpoints(urlPrefix, endpoints);
+	}
+
+	@Override
+	public void setConfig(String key, Object value) {
+		serverConfigRepository.upsert(key, value).block();
+	}
+
+	@Override
+	public Object getConfig(String key) {
+		return serverConfigRepository.findByKey(key).block();
 	}
 }
