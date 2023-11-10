32011a0dce8847ad2928bc92ac4c261b0694a8fb
FalkWolsky <fw@falkwolsky.com>
2023-11-10 19:51:15 +0100

    fix branding by ENV 2

--- client/packages/lowcoder/src/assets/images/index.tsx
+++ client/packages/lowcoder/src/assets/images/index.tsx
@@ -1,6 +1,6 @@
 //window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
-import { ReactComponent as LogoIcon } from "./logo.svg";
-import { ReactComponent as LogoWithNameIcon } from "./logo-with-name.svg";
+import { ReactComponent as LogoIcon } from "./logo-with-name-home.svg";
+import { ReactComponent as LogoWithNameIcon } from "./logo-with-name-home.svg";
 import { ReactComponent as LogoHomeIcon } from "./logo-with-name-home.svg";
 
 export { default as favicon } from "./favicon.ico";
