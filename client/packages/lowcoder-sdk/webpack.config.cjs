0d34edd51c9cbb359432ea5ee9f97c83347977d4
RAHEEL <mraheeliftikhar1994@gmail.com>
2024-03-20 16:37:38 +0500

    webpack configurations

--- client/packages/lowcoder-sdk/webpack.config.cjs
+++ client/packages/lowcoder-sdk/webpack.config.cjs
@@ -1,79 +1,11 @@
 const path = require("path");
 const TerserPlugin = require('terser-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-// const RemarkHTML = require("remark-html")
 const webpack = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const JSEntryWebpackPlugin = require('./plugins/JSEntryWebpackPlugin.cjs');
+
 const { buildVars } = require("./src/dev-utils/buildVars.cjs");
-
-// import path from "path";
-// import { fileURLToPath } from 'url';
-// import TerserPlugin from "terser-webpack-plugin";
-// import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
-// // const RemarkHTML = require("remark-html")
-// import webpack from "webpack";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const buildVars = [
-//   {
-//     name: "PUBLIC_URL",
-//     defaultValue: "/",
-//   },
-//   {
-//     name: "REACT_APP_EDITION",
-//     defaultValue: "community",
-//   },
-//   {
-//     name: "REACT_APP_LANGUAGES",
-//     defaultValue: "",
-//   },
-//   {
-//     name: "REACT_APP_COMMIT_ID",
-//     defaultValue: "00000",
-//   },
-//   {
-//     name: "REACT_APP_API_HOST",
-//     defaultValue: "",
-//   },
-//   {
-//     name: "LOWCODER_NODE_SERVICE_URL",
-//     defaultValue: "",
-//   },
-//   {
-//     name: "REACT_APP_ENV",
-//     defaultValue: "production",
-//   },
-//   {
-//     name: "REACT_APP_BUILD_ID",
-//     defaultValue: "",
-//   },
-//   {
-//     name: "REACT_APP_LOG_LEVEL",
-//     defaultValue: "error",
-//   },
-//   {
-//     name: "REACT_APP_IMPORT_MAP",
-//     defaultValue: "{}",
-//   },
-//   {
-//     name: "REACT_APP_SERVER_IPS",
-//     defaultValue: "",
-//   },
-//   {
-//     name: "REACT_APP_BUNDLE_BUILTIN_PLUGIN",
-//     defaultValue: "",
-//   },
-//   {
-//     name: "REACT_APP_BUNDLE_TYPE",
-//     defaultValue: "app",
-//   },
-//   {
-//     name: "REACT_APP_DISABLE_JS_SANDBOX",
-//     defaultValue: "",
-//   },
-// ];
 
 const define = {};
 buildVars.forEach(({ name, defaultValue }) => {
@@ -83,7 +15,7 @@ buildVars.forEach(({ name, defaultValue }) => {
 const apiBaseUrl = "http://localhost:8000";
 
 module.exports = {
-  stats: 'verbose',
+  // stats: 'verbose',
   mode: 'production',
   entry: "./index-bundle.jsx",
   externals: {
@@ -165,12 +97,12 @@ module.exports = {
     })]
   },
   output: {
-    // path: __dirname + "/dist",
     path: path.resolve(__dirname, 'bundle'),
     // publicPath: "https://sdk.lowcoder.cloud/",
     publicPath: "/",
     // filename: "bundle.js",
     filename: '[name].bundle.js',
+    clean: true,
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -181,7 +113,11 @@ module.exports = {
     new webpack.IgnorePlugin({
       resourceRegExp: /.test.(ts|tsx)$/,
     }),
-    new BundleAnalyzerPlugin()
+    new JSEntryWebpackPlugin({
+      path: path.resolve(__dirname, 'bundle'),
+      filename: 'bundle.js'
+    }),
+    // new BundleAnalyzerPlugin()
   ],
   optimization: {
     minimize: true,
@@ -192,75 +128,7 @@ module.exports = {
     splitChunks: {
       chunks: 'all',
     },
-    // splitChunks: {
-    //   cacheGroups: {
-    //     vendor: {
-    //       test: /[\\/]node_modules[\\/]/,
-    //       name: 'vendors',
-    //       chunks: 'all',
-    //     },
-    //   },
-    // },
-    // splitChunks: {
-    //   chunks: 'all',
-    //   minSize: 10000,
-    //   minRemainingSize: 0,
-    //   minChunks: 1,
-    //   maxAsyncRequests: 30,
-    //   maxInitialRequests: 30,
-    //   enforceSizeThreshold: 50000,
-    //   cacheGroups: {
-    //     default: {
-    //       minChunks: 2,
-    //       priority: -20,
-    //       reuseExistingChunk: true,
-    //     },
-    //     defaultVendors: {
-    //       test: /[\\/]node_modules[\\/]/,
-    //       priority: -10,
-    //       reuseExistingChunk: true,
-    //       // name(module) {
-    //       //   // get the name. E.g. node_modules/packageName/not/this/part.js
-    //       //   // or node_modules/packageName
-    //       //   const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-    //       //   // if (packageName === 'antd') {
-    //       //   //   return 'antd';
-    //       //   // }
-    //       //   // if (packageName === 'antd-mobile') {
-    //       //   //   return 'antd-mobile';
-    //       //   // }
-    //       //   // if (packageName === 'lodash') {
-    //       //   //   return 'lodash';
-    //       //   // }
-    //       //   // if (packageName === 'moment') {
-    //       //   //   return 'moment';
-    //       //   // }
-    //       //   // if (packageName === 'dayjs') {
-    //       //   //   return 'dayjs';
-    //       //   // }
-    //       //   // npm package names are URL-safe, but some servers don't like @ symbols
-    //       //   // return `npm.${packageName.replace('@', '')}`;
-    //       //   // return `npm.${packageName.replace('@', '')}`;
-    //       //   return `vendor`;
-    //       // },
-    //     },
-    //   },
-    // },
     runtimeChunk: 'single',
-    // splitChunks: {
-    //   chunks: 'all',
-    // }
-    // splitChunks: {
-    //   minSize: 0,
-    //   cacheGroups: {
-    //     // reuseExistingChunk: true,
-    //     vendor: {
-    //       test: /[\\/]node_modules[\\/]/,
-    //       name: 'vendors',
-    //       chunks: 'all'
-    //     }
-    //   }
-    // }
   },
   devServer: {
     static: {
