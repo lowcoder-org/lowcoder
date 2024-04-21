cd682d1745c2921a31f246e70a4e38592c20b061
FalkWolsky <fw@falkwolsky.com>
2024-04-21 14:30:12 +0200

    English Standard, German Improvements

--- client/packages/lowcoder/src/i18n/index.tsx
+++ client/packages/lowcoder/src/i18n/index.tsx
@@ -4,6 +4,7 @@ import { I18nObjects } from "./locales/types";
 
 export const { trans, transToNode, language } = new Translator<typeof localeData.en>(
   localeData,
-  REACT_APP_LANGUAGES
+  REACT_APP_LANGUAGES,
+  ['de']
 );
 export const i18nObjs = getI18nObjects<I18nObjects>(localeData, REACT_APP_LANGUAGES);
--- client/packages/lowcoder/src/i18n/locales/index.ts
+++ client/packages/lowcoder/src/i18n/locales/index.ts
@@ -1,6 +1,6 @@
 // file examples: en, enGB, zh, zhHK
 // fallback example: current locale is zh-HK, fallback order is zhHK => zh => en
-// export * from "./de"; Not ready yet
+export * from "./de"; 
 export * from "./en";
 export * from "./it";
 export * from "@lowcoder-ee/i18n/locales/zh";
