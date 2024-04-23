f7e3a074d4d84ac67a67fc1c68687e0852945dd4
MenaamAfzal <officialmeenamafzal@gmail.com>
2024-04-23 22:11:45 +0500

    echarts and map integrated

--- client/packages/lowcoder-comps/src/index.ts
+++ client/packages/lowcoder-comps/src/index.ts
@@ -2,9 +2,13 @@ import { ChartCompWithDefault } from "./comps/chartComp/chartComp";
 import { ImageEditorComp } from "./comps/imageEditorComp/index";
 import { CalendarComp } from "./comps/calendarComp/calendarComp";
 import { MermaidComp } from "comps/mermaidComp"; 
+import { EChartsCompWithDefault } from "comps/eChartsComp/echartsComp";
+import { MapCompWithDefault } from "comps/mapComp/mapComp";
 
 export default {
   chart: ChartCompWithDefault,
+  map: MapCompWithDefault,
+  eCharts: EChartsCompWithDefault,
   imageEditor: ImageEditorComp,
   calendar: CalendarComp,
   mermaid: MermaidComp,
