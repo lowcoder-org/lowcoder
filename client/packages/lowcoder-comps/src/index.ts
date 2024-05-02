cdb09995e8fc0dda7c6689e77219b86857d52147
MenaamAfzal <officialmeenamafzal@gmail.com>
2024-05-03 00:03:59 +0500

    charts displayed

--- client/packages/lowcoder-comps/src/index.ts
+++ client/packages/lowcoder-comps/src/index.ts
@@ -2,13 +2,17 @@ import { ChartCompWithDefault } from "./comps/chartComp/chartComp";
 import { ImageEditorComp } from "./comps/imageEditorComp/index";
 import { CalendarComp } from "./comps/calendarComp/calendarComp";
 import { MermaidComp } from "comps/mermaidComp"; 
-import { EChartsCompWithDefault } from "comps/eChartsComp/echartsComp";
 import { MapCompWithDefault } from "comps/mapComp/mapComp";
+import { FunnelChartCompWithDefault } from "comps/chartComp/funnelChartComp/funnelChartComp";
+import { GaugeChartCompWithDefault } from "comps/chartComp/gaugeChartComp/gaugeChartComp";
+import { SankeyChartCompWithDefault } from "comps/chartComp/sankeyChartComp/sankeyChartComp";
 
 export default {
   chart: ChartCompWithDefault,
   map: MapCompWithDefault,
-  eCharts: EChartsCompWithDefault,
+  funnelChart: FunnelChartCompWithDefault,
+  gaugeChart: GaugeChartCompWithDefault,
+  sankeyChart: SankeyChartCompWithDefault,
   imageEditor: ImageEditorComp,
   calendar: CalendarComp,
   mermaid: MermaidComp,
