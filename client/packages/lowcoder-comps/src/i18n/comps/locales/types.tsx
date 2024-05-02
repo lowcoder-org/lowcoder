8b48b9145bf960cdfa71ac23dec203bc83f56658
MenaamAfzal <officialmeenamafzal@gmail.com>
2024-05-03 00:00:11 +0500

    new types added

--- client/packages/lowcoder-comps/src/i18n/comps/locales/types.tsx
+++ client/packages/lowcoder-comps/src/i18n/comps/locales/types.tsx
@@ -4,6 +4,9 @@ import { XAXisComponentOption } from "echarts";
 export type I18nObjects = {
   defaultDataSource: JSONObject[];
   defaultEchartsJsonOption: Record<string, unknown>;
+  defaultGaugeChartOption: Record<string, unknown>;
+  defaultFunnelChartOption: Record<string, unknown>;
+  defaultSankeyChartOption: Record<string, unknown>;
   defaultMapJsonOption: Record<string, unknown>;
   timeXAxisLabel?: XAXisComponentOption["axisLabel"];
   imageEditorLocale?: Record<string, string>;
