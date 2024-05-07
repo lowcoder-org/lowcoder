e7a50697072f5946d32f6e249f8a6947c81e9d88
FalkWolsky <fw@falkwolsky.com>
2024-05-07 21:05:30 +0200

    Fixing Paths for remote components

--- client/packages/lowcoder-comps/src/index.ts
+++ client/packages/lowcoder-comps/src/index.ts
@@ -1,23 +1,23 @@
 import { ChartCompWithDefault } from "./comps/chartComp/chartComp";
 import { ImageEditorComp } from "./comps/imageEditorComp/index";
 import { CalendarComp } from "./comps/calendarComp/calendarComp";
-import { ChartsGeoMapComp } from "comps/chartsGeoMapComp/chartsGeoMapComp";
-import { FunnelChartCompWithDefault } from "comps/funnelChartComp/funnelChartComp";
-import { GaugeChartCompWithDefault } from "comps/gaugeChartComp/gaugeChartComp";
-import { SankeyChartCompWithDefault } from "comps/sankeyChartComp/sankeyChartComp";
-import { CandleStickChartCompWithDefault } from "comps/candleStickChartComp/candleStickChartComp";
-import { RadarChartCompWithDefault } from "comps/radarChartComp/radarChartComp";
-import { HeatmapChartCompWithDefault } from "comps/heatmapChartComp/heatmapChartComp";
-import { GraphChartCompWithDefault } from "comps/graphChartComp/graphChartComp";
-import { TreeChartCompWithDefault } from "comps/treeChartComp/treechartComp";
-import { TreemapChartCompWithDefault } from "comps/treemapChartComp/treemapChartComp";
-import { SunburstChartCompWithDefault } from "comps/sunburstChartComp/sunburstChartComp";
-import { ThemeriverChartCompWithDefault } from "comps/themeriverChartComp/themeriverChartComp";
+import { ChartsGeoMapComp } from "./comps/chartsGeoMapComp/chartsGeoMapComp";
+import { FunnelChartCompWithDefault } from "./comps/funnelChartComp/funnelChartComp";
+import { GaugeChartCompWithDefault } from "./comps/gaugeChartComp/gaugeChartComp";
+import { SankeyChartCompWithDefault } from "./comps/sankeyChartComp/sankeyChartComp";
+import { CandleStickChartCompWithDefault } from "./comps/candleStickChartComp/candleStickChartComp";
+import { RadarChartCompWithDefault } from "./comps/radarChartComp/radarChartComp";
+import { HeatmapChartCompWithDefault } from "./comps/heatmapChartComp/heatmapChartComp";
+import { GraphChartCompWithDefault } from "./comps/graphChartComp/graphChartComp";
+import { TreeChartCompWithDefault } from "./comps/treeChartComp/treechartComp";
+import { TreemapChartCompWithDefault } from "./comps/treemapChartComp/treemapChartComp";
+import { SunburstChartCompWithDefault } from "./comps/sunburstChartComp/sunburstChartComp";
+import { ThemeriverChartCompWithDefault } from "./comps/themeriverChartComp/themeriverChartComp";
 
-import { MermaidComp } from "comps/mermaidComp";
-import { MeetingControllerComp } from "comps/agoraMeetingComp/meetingControllerComp";
-import { VideoMeetingStreamComp } from "comps/agoraMeetingComp/videoMeetingStreamComp";
-import { VideoSharingStreamComp } from "comps/agoraMeetingComp/videoSharingStreamComp";
+import { MermaidComp } from "./comps/mermaidComp";
+import { MeetingControllerComp } from "./comps/agoraMeetingComp/meetingControllerComp";
+import { VideoMeetingStreamComp } from "./comps/agoraMeetingComp/videoMeetingStreamComp";
+import { VideoSharingStreamComp } from "./comps/agoraMeetingComp/videoSharingStreamComp";
 
 export default {
   chart: ChartCompWithDefault,
