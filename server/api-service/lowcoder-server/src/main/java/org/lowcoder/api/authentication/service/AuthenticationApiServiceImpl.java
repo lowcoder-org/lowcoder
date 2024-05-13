f17a5205858fe4ae0efed74c10e7750fd7ff1ca9
th37rose <thomasr37oss@gmail.com>
2024-05-13 07:47:24 +0000

    Enable adding auth provider with same authType

--- server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/authentication/service/AuthenticationApiServiceImpl.java
+++ server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/authentication/service/AuthenticationApiServiceImpl.java
@@ -418,12 +418,6 @@ public class AuthenticationApiServiceImpl implements AuthenticationApiService {
                 .stream()
                 .collect(Collectors.toMap(AbstractAuthConfig::getId, Function.identity()));
 
-        boolean authTypeAlreadyExists = authConfigMap.values().stream()
-                .anyMatch(config -> !config.getId().equals(newAuthConfig.getId()) && config.getAuthType().equals(newAuthConfig.getAuthType()));
-        if(authTypeAlreadyExists) {
-            return false;
-        }
-
         // Under the organization, the source can uniquely identify the whole auth config.
         AbstractAuthConfig old = authConfigMap.get(newAuthConfig.getId());
         if (old != null) {
