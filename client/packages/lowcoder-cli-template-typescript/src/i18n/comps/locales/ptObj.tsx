cabca98a095b7b4bf9022968cc94d9c99d8e7044
Carlos Feitosa <71650034+cadufeitosa@users.noreply.github.com>
2024-05-15 19:32:50 -0400

    Initial PT translations

---
+++ client/packages/lowcoder-cli-template-typescript/src/i18n/comps/locales/ptObj.tsx
@@ -0,0 +1,40 @@
+import { I18nObjects } from "./types";
+
+export const enObj: I18nObjects = {
+  defaultData: [
+    {
+      id : 1,
+      color: 'gray',
+      description: 'Validação: Integração do Salesforce',
+      x: 25,
+      size: 15
+    },
+    { 
+      id : 2,
+      color: 'maroon', 
+      description: 'Renovações',
+      x: 80, 
+      size: 10 },
+    {
+      id : 3,
+      color: 'maroon',
+      description: 'Rafatoramento: Fancy Pants',
+      x: 35,
+      size: 10
+    },
+    {
+      id : 4,
+      color: 'cyan',
+      description: 'Refatoramento: Lighthouse Orbs',
+      x: 45,
+      size: 10
+    },
+    {
+      id : 5,
+      color: 'yellow',
+      description: 'Migração e Atualização de Dados',
+      x: 50,
+      size: 20
+    }
+  ],
+};
