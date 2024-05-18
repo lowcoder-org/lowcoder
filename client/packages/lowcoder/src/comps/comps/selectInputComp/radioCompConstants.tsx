79a7d590e342b616bb8834d9286d999e81567290
MeenamAfzal <meenamafzal1@gmail.com>
2024-05-19 02:20:37 +0500

    formating removed-64

--- client/packages/lowcoder/src/comps/comps/selectInputComp/radioCompConstants.tsx
+++ client/packages/lowcoder/src/comps/comps/selectInputComp/radioCompConstants.tsx
@@ -1,43 +1,32 @@
-import {RecordConstructorToComp} from 'lowcoder-core';
-import {BoolCodeControl} from '../../controls/codeControl';
-import {LabelControl} from '../../controls/labelControl';
+import { RecordConstructorToComp } from "lowcoder-core";
+import { BoolCodeControl } from "../../controls/codeControl";
+import { LabelControl } from "../../controls/labelControl";
 import {
   arrayStringExposingStateControl,
   stringExposingStateControl,
-} from '../../controls/codeStateControl';
-import {Section, sectionNames} from 'lowcoder-design';
-import {SelectInputOptionControl} from '../../controls/optionsControl';
-import {ChangeEventHandlerControl} from '../../controls/eventHandlerControl';
+} from "../../controls/codeStateControl";
+import { Section, sectionNames } from "lowcoder-design";
+import { SelectInputOptionControl } from "../../controls/optionsControl";
+import { ChangeEventHandlerControl } from "../../controls/eventHandlerControl";
 import {
   SelectInputValidationChildren,
   SelectInputValidationSection,
-} from './selectInputConstants';
-import {
-  formDataChildren,
-  FormDataPropertyView,
-} from '../formComp/formDataConstants';
-import {styleControl} from 'comps/controls/styleControl';
-import {
-  AnimationStyle,
-  InputFieldStyle,
-  LabelStyle,
-  RadioStyle,
-} from 'comps/controls/styleControlConstants';
-import {dropdownControl} from '../../controls/dropdownControl';
-import {
-  hiddenPropertyView,
-  disabledPropertyView,
-} from 'comps/utils/propertyUtils';
-import {trans} from 'i18n';
-import {RefControl} from 'comps/controls/refControl';
+} from "./selectInputConstants";
+import { formDataChildren, FormDataPropertyView } from "../formComp/formDataConstants";
+import { styleControl } from "comps/controls/styleControl";
+import {  AnimationStyle, InputFieldStyle, LabelStyle, RadioStyle } from "comps/controls/styleControlConstants";
+import { dropdownControl } from "../../controls/dropdownControl";
+import { hiddenPropertyView, disabledPropertyView } from "comps/utils/propertyUtils";
+import { trans } from "i18n";
+import { RefControl } from "comps/controls/refControl";
 
-import {useContext} from 'react';
-import {EditorContext} from 'comps/editorState';
+import { useContext } from "react";
+import { EditorContext } from "comps/editorState";
 
 export const RadioLayoutOptions = [
-  {label: trans('radio.horizontal'), value: 'horizontal'},
-  {label: trans('radio.vertical'), value: 'vertical'},
-  {label: trans('radio.autoColumns'), value: 'auto_columns'},
+  { label: trans("radio.horizontal"), value: "horizontal" },
+  { label: trans("radio.vertical"), value: "vertical" },
+  { label: trans("radio.autoColumns"), value: "auto_columns" },
 ] as const;
 
 export const RadioChildrenMap = {
@@ -59,7 +48,7 @@ export const RadioChildrenMap = {
 
 export const RadioPropertyView = (
   children: RecordConstructorToComp<
-    typeof RadioChildrenMap & {hidden: typeof BoolCodeControl} & {
+    typeof RadioChildrenMap & { hidden: typeof BoolCodeControl } & {
       defaultValue:
         | ReturnType<typeof stringExposingStateControl>
         | ReturnType<typeof arrayStringExposingStateControl>;
@@ -72,57 +61,44 @@ export const RadioPropertyView = (
   <>
     <Section name={sectionNames.basic}>
       {children.options.propertyView({})}
-      {children.defaultValue.propertyView({label: trans('prop.defaultValue')})}
+      {children.defaultValue.propertyView({ label: trans("prop.defaultValue") })}
     </Section>
 
-    {['logic', 'both'].includes(useContext(EditorContext).editorModeStatus) && (
-      <>
-        <SelectInputValidationSection {...children} />
-        <FormDataPropertyView {...children} />
-        <Section name={sectionNames.interaction}>
-          {children.onEvent.getPropertyView()}
-          {disabledPropertyView(children)}
-          {hiddenPropertyView(children)}
-        </Section>
-      </>
+    {["logic", "both"].includes(useContext(EditorContext).editorModeStatus) && (
+      <><SelectInputValidationSection {...children} />
+      <FormDataPropertyView {...children} />
+      <Section name={sectionNames.interaction}>
+        {children.onEvent.getPropertyView()}
+        {disabledPropertyView(children)}
+        {hiddenPropertyView(children)}
+      </Section></>
     )}
 
-    {['layout', 'both'].includes(
-      useContext(EditorContext).editorModeStatus
-    ) && (
+    {["layout", "both"].includes(useContext(EditorContext).editorModeStatus) && (
       <Section name={sectionNames.layout}>
         {children.layout.propertyView({
-          label: trans('radio.options'),
+          label: trans("radio.options"),
           tooltip: (
-            <div style={{display: 'flex', flexDirection: 'column', gap: '8px'}}>
-              <div>{trans('radio.horizontalTooltip')}</div>
-              <div>{trans('radio.verticalTooltip')}</div>
-              <div>{trans('radio.autoColumnsTooltip')}</div>
+            <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+              <div>{trans("radio.horizontalTooltip")}</div>
+              <div>{trans("radio.verticalTooltip")}</div>
+              <div>{trans("radio.autoColumnsTooltip")}</div>
             </div>
           ),
         })}
       </Section>
     )}
 
-    {['layout', 'both'].includes(useContext(EditorContext).editorModeStatus) &&
-      children.label.getPropertyView()}
+    {["layout", "both"].includes(useContext(EditorContext).editorModeStatus) && ( 
+      children.label.getPropertyView() 
+    )}
 
-    {['layout', 'both'].includes(
-      useContext(EditorContext).editorModeStatus
-    ) && (
+    {["layout", "both"].includes(useContext(EditorContext).editorModeStatus) && (
       <>
-        <Section name={sectionNames.style}>
-          {children.style.getPropertyView()}
-        </Section>
-        <Section name={sectionNames.labelStyle}>
-          {children.labelStyle.getPropertyView()}
-        </Section>
-        <Section name={sectionNames.inputFieldStyle}>
-          {children.inputFieldStyle.getPropertyView()}
-        </Section>
-        <Section name={sectionNames.animationStyle}>
-          {children.animationStyle.getPropertyView()}
-        </Section>
+      <Section name={sectionNames.style}>{children.style.getPropertyView()}</Section>
+      <Section name={sectionNames.labelStyle}>{children.labelStyle.getPropertyView()}</Section>
+      <Section name={sectionNames.inputFieldStyle}>{children.inputFieldStyle.getPropertyView()}</Section>
+      <Section name={sectionNames.animationStyle}>{children.animationStyle.getPropertyView()}</Section>
       </>
     )}
   </>
