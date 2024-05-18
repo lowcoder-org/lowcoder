b97caf0c7b39c3d60715bdf1dcc79f7c9a13add2
MeenamAfzal <meenamafzal1@gmail.com>
2024-05-19 01:24:55 +0500

    formating removed-34

--- client/packages/lowcoder/src/comps/comps/containerComp/pageLayoutComp.tsx
+++ client/packages/lowcoder/src/comps/comps/containerComp/pageLayoutComp.tsx
@@ -6,14 +6,17 @@ import { NameGenerator } from "comps/utils/nameGenerator";
 import { Section, sectionNames } from "lowcoder-design";
 import { oldContainerParamsToNew } from "../containerBase";
 import { toSimpleContainerData } from "../containerBase/simpleContainerComp";
-import { disabledPropertyView, hiddenPropertyView, } from "comps/utils/propertyUtils";
+import { disabledPropertyView, hiddenPropertyView } from "comps/utils/propertyUtils";
 import { trans } from "i18n";
 import { BoolCodeControl } from "comps/controls/codeControl";
 import { DisabledContext } from "comps/generators/uiCompBuilder";
 import React, { useContext, useEffect, useState } from "react";
 import { EditorContext } from "comps/editorState";
 
-import { ContainerChildren, ContainerCompBuilder } from "../pageLayoutComp/pageLayoutCompBuilder";
+import {
+  ContainerChildren,
+  ContainerCompBuilder,
+} from "../pageLayoutComp/pageLayoutCompBuilder";
 import { PageLayout } from "../pageLayoutComp/pageLayout";
 import { AnimationStyle, styleControl } from "@lowcoder-ee/index.sdk";
 
@@ -24,58 +27,52 @@ export const ContainerBaseComp = (function () {
   };
 
   return new ContainerCompBuilder(childrenMap, (props, dispatch) => {
+
     const [siderCollapsed, setSiderCollapsed] = useState(false);
 
     return (
       <DisabledContext.Provider value={props.disabled}>
-        <PageLayout
-          {...props}
-          siderCollapsed={siderCollapsed}
-          setSiderCollapsed={setSiderCollapsed}
-        />
+        <PageLayout {...props} siderCollapsed={siderCollapsed} setSiderCollapsed={setSiderCollapsed} />
       </DisabledContext.Provider>
     );
   })
     .setPropertyViewFn((children) => {
       return (
         <>
-          {(useContext(EditorContext).editorModeStatus === "logic" ||
-            useContext(EditorContext).editorModeStatus === "both") && (
+          {(useContext(EditorContext).editorModeStatus === "logic" || useContext(EditorContext).editorModeStatus === "both") && (
             <Section name={sectionNames.interaction}>
               {disabledPropertyView(children)}
               {hiddenPropertyView(children)}
-              {children.container.appSelectorPropertyView()}
+              { children.container.appSelectorPropertyView()}
             </Section>
           )}
 
-          {(useContext(EditorContext).editorModeStatus === "layout" ||
-            useContext(EditorContext).editorModeStatus === "both") && (
-            <>
-              <Section name={sectionNames.layout}>
-                {children.container.getPropertyView()}
-              </Section>
-              <Section name={sectionNames.style}>
-                {children.container.stylePropertyView()}
+          {(useContext(EditorContext).editorModeStatus === "layout" || useContext(EditorContext).editorModeStatus === "both") && (
+            <><Section name={sectionNames.layout}>
+              {children.container.getPropertyView()}
+            </Section>
+            <Section name={sectionNames.style}>
+              { children.container.stylePropertyView() }
               </Section>
               <Section name={sectionNames.animationStyle}>
                 {children.animationStyle.getPropertyView()}
               </Section>
-              {children.container.children.showHeader.getView() && (
-                <Section name={"Header Style"}>
-                  {children.container.headerStylePropertyView()}
+            {children.container.children.showHeader.getView() && (
+              <Section name={"Header Style"}>
+                { children.container.headerStylePropertyView() }
                 </Section>
               )}
-              {children.container.children.showSider.getView() && (
-                <Section name={"Sider Style"}>
-                  {children.container.siderStylePropertyView()}
+            {children.container.children.showSider.getView() && (
+              <Section name={"Sider Style"}>
+                { children.container.siderStylePropertyView() }
                 </Section>
               )}
-              <Section name={"Body Style"}>
-                {children.container.bodyStylePropertyView()}
-              </Section>
-              {children.container.children.showFooter.getView() && (
-                <Section name={"Footer Style"}>
-                  {children.container.footerStylePropertyView()}
+            <Section name={"Body Style"}>
+              { children.container.bodyStylePropertyView() }
+            </Section>
+            {children.container.children.showFooter.getView() && (
+              <Section name={"Footer Style"}>
+                { children.container.footerStylePropertyView() }
                 </Section>
               )}
             </>
