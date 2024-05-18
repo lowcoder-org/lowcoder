756951831c7e77a4331a48fc7964ea6252b94a72
MeenamAfzal <meenamafzal1@gmail.com>
2024-05-19 01:33:50 +0500

    formating removed-39

--- client/packages/lowcoder/src/comps/comps/containerComp/textContainerComp.tsx
+++ client/packages/lowcoder/src/comps/comps/containerComp/textContainerComp.tsx
@@ -1,67 +1,67 @@
-import {BoolCodeControl, StringControl} from 'comps/controls/codeControl';
-import {stringExposingStateControl} from 'comps/controls/codeStateControl';
-import {ToDataType} from 'comps/generators/multi';
+import { BoolCodeControl, StringControl } from "comps/controls/codeControl";
+import { stringExposingStateControl } from "comps/controls/codeStateControl";
+import { ToDataType } from "comps/generators/multi";
 import {
   NameConfigHidden,
   withExposingConfigs,
-} from 'comps/generators/withExposing';
-import {NameGenerator} from 'comps/utils/nameGenerator';
-import {hiddenPropertyView} from 'comps/utils/propertyUtils';
-import {trans} from 'i18n';
-import {CompParams} from 'lowcoder-core';
-import {Section, sectionNames} from 'lowcoder-design';
-import {oldContainerParamsToNew} from '../containerBase';
-import {toSimpleContainerData} from '../containerBase/simpleContainerComp';
+} from "comps/generators/withExposing";
+import { NameGenerator } from "comps/utils/nameGenerator";
+import { hiddenPropertyView } from "comps/utils/propertyUtils";
+import { trans } from "i18n";
+import { CompParams } from "lowcoder-core";
+import { Section, sectionNames } from "lowcoder-design";
+import { oldContainerParamsToNew } from "../containerBase";
+import { toSimpleContainerData } from "../containerBase/simpleContainerComp";
 import {
   ContainerChildren,
   ContainerCompBuilder,
-} from '../triContainerComp/triContainerCompBuilder';
-import {TriContainer} from '../triContainerComp/triFloatTextContainer';
-import {dropdownControl} from 'comps/controls/dropdownControl';
-import {withDefault} from 'comps/generators/simpleGenerators';
-import {styleControl} from 'comps/controls/styleControl';
-import {AnimationStyle, TextStyle} from 'comps/controls/styleControlConstants';
-import {useContext} from 'react';
-import {EditorContext} from 'comps/editorState';
-import {alignWithJustifyControl} from 'comps/controls/alignControl';
+} from "../triContainerComp/triContainerCompBuilder";
+import { TriContainer } from "../triContainerComp/triFloatTextContainer";
+import { dropdownControl } from "comps/controls/dropdownControl";
+import { withDefault } from "comps/generators/simpleGenerators";
+import { styleControl } from "comps/controls/styleControl";
+import { AnimationStyle, TextStyle } from "comps/controls/styleControlConstants";
+import { useContext } from "react";
+import { EditorContext } from "comps/editorState";
+import { alignWithJustifyControl } from "comps/controls/alignControl";
 
 const typeOptions = [
   {
-    label: 'Markdown',
-    value: 'markdown',
+    label: "Markdown",
+    value: "markdown",
   },
   {
-    label: trans('text'),
-    value: 'text',
+    label: trans("text"),
+    value: "text",
   },
 ] as const;
 
 const floatOptions = [
   {
-    label: 'None',
-    value: 'none',
+    label: "None",
+    value: "none",
   },
   {
-    label: 'Right',
-    value: 'right',
+    label: "Right",
+    value: "right",
   },
   {
-    label: 'Left',
-    value: 'left',
+    label: "Left",
+    value: "left",
   },
 ] as const;
+
 
 export const ContainerBaseComp = (function () {
   const childrenMap = {
     disabled: BoolCodeControl,
     text: stringExposingStateControl(
-      'text',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat faucibus eleifend. Pellentesque eleifend, risus vel sagittis mattis, mauris ipsum tempor sapien, eu lobortis lacus libero a dui. Cras erat felis, rhoncus vestibulum consectetur et, ultrices ut purus. Sed a tortor orci. Vestibulum nec eleifend ante.'
+      "text", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat faucibus eleifend. Pellentesque eleifend, risus vel sagittis mattis, mauris ipsum tempor sapien, eu lobortis lacus libero a dui. Cras erat felis, rhoncus vestibulum consectetur et, ultrices ut purus. Sed a tortor orci. Vestibulum nec eleifend ante."
     ),
-    type: dropdownControl(typeOptions, 'markdown'),
-    float: dropdownControl(floatOptions, 'left'),
+    type: dropdownControl(typeOptions, "markdown"),
+    float: dropdownControl(floatOptions, "left"),
     horizontalAlignment: alignWithJustifyControl(),
-    width: withDefault(StringControl, '40'),
+    width: withDefault(StringControl, "40"),
     style: styleControl(TextStyle),
     animationStyle: styleControl(AnimationStyle),
   };
@@ -72,61 +72,49 @@ export const ContainerBaseComp = (function () {
       return (
         <>
           <Section name={sectionNames.basic}>
-            {children.type.propertyView({
-              label: trans('value'),
-              tooltip: trans('textShow.valueTooltip'),
-              radioButton: true,
-            })}
+            {children.type.propertyView({label: trans("value"), tooltip: trans("textShow.valueTooltip"), radioButton: true,})}
             {children.text.propertyView({})}
           </Section>
 
-          {['logic', 'both'].includes(
-            useContext(EditorContext).editorModeStatus
-          ) && (
+          {["logic", "both"].includes(useContext(EditorContext).editorModeStatus) && (
             <Section name={sectionNames.interaction}>
               {hiddenPropertyView(children)}
             </Section>
           )}
-          {['layout', 'both'].includes(
-            useContext(EditorContext).editorModeStatus
-          ) && (
+          {["layout", "both"].includes(useContext(EditorContext).editorModeStatus) && (
             <>
               <Section name={sectionNames.layout}>
                 {children.container.getPropertyView()}
-                {children.width.propertyView({
-                  label: trans('container.flowWidth'),
-                })}
-                {children.float.propertyView({
-                  label: trans('container.floatType'),
-                  radioButton: true,
+                {children.width.propertyView({label: trans("container.flowWidth")})}
+                {children.float.propertyView({ label: trans("container.floatType"), radioButton: true,
                 })}
                 {children.horizontalAlignment.propertyView({
-                  label: trans('textShow.horizontalAlignment'),
+                  label: trans("textShow.horizontalAlignment"),
                   radioButton: true,
                 })}
               </Section>
-              <Section name={'Floating Text Style'}>
+              <Section name={"Floating Text Style"}>
                 {children.style.getPropertyView()}
               </Section>
               <Section name={sectionNames.animationStyle}>
                 {children.animationStyle.getPropertyView()}
               </Section>
-              <Section name={'Container Style'}>
+              <Section name={"Container Style"}>
                 {children.container.stylePropertyView()}
               </Section>
               {children.container.children.showHeader.getView() && (
-                <Section name={'Header Style'}>
-                  {children.container.headerStylePropertyView()}
+                <Section name={"Header Style"}>
+                  { children.container.headerStylePropertyView() }
                 </Section>
               )}
               {children.container.children.showBody.getView() && (
-                <Section name={'Body Style'}>
-                  {children.container.bodyStylePropertyView()}
+                <Section name={"Body Style"}>
+                  { children.container.bodyStylePropertyView() }
                 </Section>
               )}
               {children.container.children.showFooter.getView() && (
-                <Section name={'Footer Style'}>
-                  {children.container.footerStylePropertyView()}
+                <Section name={"Footer Style"}>
+                  { children.container.footerStylePropertyView() }
                 </Section>
               )}
             </>
@@ -147,7 +135,7 @@ function convertOldContainerParams(params: CompParams<any>) {
     // old params
     if (
       container &&
-      (container.hasOwnProperty('layout') || container.hasOwnProperty('items'))
+      (container.hasOwnProperty("layout") || container.hasOwnProperty("items"))
     ) {
       const autoHeight = tempParams.value.autoHeight;
       return {
@@ -155,7 +143,7 @@ function convertOldContainerParams(params: CompParams<any>) {
         value: {
           container: {
             showHeader: false,
-            body: {0: {view: container}},
+            body: { 0: { view: container } },
             showBody: true,
             showFooter: false,
             autoHeight: autoHeight,
@@ -188,21 +176,21 @@ export function defaultContainerData(
       header: toSimpleContainerData([
         {
           item: {
-            compType: 'text',
-            name: nameGenerator.genItemName('containerTitle'),
+            compType: "text",
+            name: nameGenerator.genItemName("containerTitle"),
             comp: {
-              text: '### ' + trans('container.title'),
+              text: "### " + trans("container.title"),
             },
           },
           layoutItem: {
-            i: '',
+            i: "",
             h: 5,
             w: 24,
             x: 0,
             y: 0,
           },
         },
-      ]),
+      ])
     },
   };
 }
