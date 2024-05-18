26fcd2e5fe8928a428f56b264fd703ae50c63644
MeenamAfzal <meenamafzal1@gmail.com>
2024-05-19 01:05:47 +0500

    formating removed-33

--- client/packages/lowcoder/src/comps/comps/containerComp/pageLayoutComp.tsx
+++ client/packages/lowcoder/src/comps/comps/containerComp/pageLayoutComp.tsx
@@ -1,33 +1,21 @@
-import {CompParams} from 'lowcoder-core';
-import {ToDataType} from 'comps/generators/multi';
-import {
-  NameConfigDisabled,
-  NameConfigHidden,
-  withExposingConfigs,
-  NameConfig,
-  CompDepsConfig,
-} from 'comps/generators/withExposing';
-import {withMethodExposing} from 'comps/generators/withMethodExposing';
-import {NameGenerator} from 'comps/utils/nameGenerator';
-import {Section, sectionNames} from 'lowcoder-design';
-import {oldContainerParamsToNew} from '../containerBase';
-import {toSimpleContainerData} from '../containerBase/simpleContainerComp';
-import {
-  disabledPropertyView,
-  hiddenPropertyView,
-} from 'comps/utils/propertyUtils';
-import {trans} from 'i18n';
-import {BoolCodeControl} from 'comps/controls/codeControl';
-import {DisabledContext} from 'comps/generators/uiCompBuilder';
-import React, {useContext, useEffect, useState} from 'react';
-import {EditorContext} from 'comps/editorState';
+import { CompParams } from "lowcoder-core";
+import { ToDataType } from "comps/generators/multi";
+import { NameConfigDisabled, NameConfigHidden, withExposingConfigs, NameConfig, CompDepsConfig } from "comps/generators/withExposing";
+import { withMethodExposing } from "comps/generators/withMethodExposing";
+import { NameGenerator } from "comps/utils/nameGenerator";
+import { Section, sectionNames } from "lowcoder-design";
+import { oldContainerParamsToNew } from "../containerBase";
+import { toSimpleContainerData } from "../containerBase/simpleContainerComp";
+import { disabledPropertyView, hiddenPropertyView, } from "comps/utils/propertyUtils";
+import { trans } from "i18n";
+import { BoolCodeControl } from "comps/controls/codeControl";
+import { DisabledContext } from "comps/generators/uiCompBuilder";
+import React, { useContext, useEffect, useState } from "react";
+import { EditorContext } from "comps/editorState";
 
-import {
-  ContainerChildren,
-  ContainerCompBuilder,
-} from '../pageLayoutComp/pageLayoutCompBuilder';
-import {PageLayout} from '../pageLayoutComp/pageLayout';
-import {AnimationStyle, styleControl} from '@lowcoder-ee/index.sdk';
+import { ContainerChildren, ContainerCompBuilder } from "../pageLayoutComp/pageLayoutCompBuilder";
+import { PageLayout } from "../pageLayoutComp/pageLayout";
+import { AnimationStyle, styleControl } from "@lowcoder-ee/index.sdk";
 
 export const ContainerBaseComp = (function () {
   const childrenMap = {
@@ -51,8 +39,8 @@ export const ContainerBaseComp = (function () {
     .setPropertyViewFn((children) => {
       return (
         <>
-          {(useContext(EditorContext).editorModeStatus === 'logic' ||
-            useContext(EditorContext).editorModeStatus === 'both') && (
+          {(useContext(EditorContext).editorModeStatus === "logic" ||
+            useContext(EditorContext).editorModeStatus === "both") && (
             <Section name={sectionNames.interaction}>
               {disabledPropertyView(children)}
               {hiddenPropertyView(children)}
@@ -60,8 +48,8 @@ export const ContainerBaseComp = (function () {
             </Section>
           )}
 
-          {(useContext(EditorContext).editorModeStatus === 'layout' ||
-            useContext(EditorContext).editorModeStatus === 'both') && (
+          {(useContext(EditorContext).editorModeStatus === "layout" ||
+            useContext(EditorContext).editorModeStatus === "both") && (
             <>
               <Section name={sectionNames.layout}>
                 {children.container.getPropertyView()}
@@ -73,20 +61,20 @@ export const ContainerBaseComp = (function () {
                 {children.animationStyle.getPropertyView()}
               </Section>
               {children.container.children.showHeader.getView() && (
-                <Section name={'Header Style'}>
+                <Section name={"Header Style"}>
                   {children.container.headerStylePropertyView()}
                 </Section>
               )}
               {children.container.children.showSider.getView() && (
-                <Section name={'Sider Style'}>
+                <Section name={"Sider Style"}>
                   {children.container.siderStylePropertyView()}
                 </Section>
               )}
-              <Section name={'Body Style'}>
+              <Section name={"Body Style"}>
                 {children.container.bodyStylePropertyView()}
               </Section>
               {children.container.children.showFooter.getView() && (
-                <Section name={'Footer Style'}>
+                <Section name={"Footer Style"}>
                   {children.container.footerStylePropertyView()}
                 </Section>
               )}
@@ -108,7 +96,7 @@ function convertOldContainerParams(params: CompParams<any>) {
     // old params
     if (
       container &&
-      (container.hasOwnProperty('layout') || container.hasOwnProperty('items'))
+      (container.hasOwnProperty("layout") || container.hasOwnProperty("items"))
     ) {
       const autoHeight = tempParams.value.autoHeight;
       const scrollbars = tempParams.value.scrollbars;
@@ -140,23 +128,23 @@ const PageLayoutCompTmP = withExposingConfigs(ContainerTmpComp, [
   NameConfigHidden,
   NameConfigDisabled,
 
-  new NameConfig('container', trans('export.ratingValueDesc')),
+  new NameConfig("container", trans("export.ratingValueDesc")),
   new CompDepsConfig(
-    'siderCollapsed',
+    "siderCollapsed",
     (comp) => ({
       data: comp.children.container.children.siderCollapsed.nodeWithoutCache(),
     }),
     (input) => input.data.value,
-    trans('listView.itemsDesc')
+    trans("listView.itemsDesc")
   ),
 ]);
 
 export const PageLayoutComp = withMethodExposing(PageLayoutCompTmP, [
   {
     method: {
-      name: 'setSiderCollapsed',
-      description: 'Set the Sider of the PageLayout to be collapsed or not',
-      params: [{name: 'collapsed', type: 'boolean'}],
+      name: "setSiderCollapsed",
+      description: "Set the Sider of the PageLayout to be collapsed or not",
+      params: [{name: "collapsed", type: "boolean"}],
     },
     execute: (comp, values) => {
       const page = values[0] as number;
@@ -178,36 +166,36 @@ export function defaultPageLayoutData(
       header: toSimpleContainerData([
         {
           item: {
-            compType: 'navigation',
-            name: nameGenerator.genItemName('pageNavigation'),
+            compType: "navigation",
+            name: nameGenerator.genItemName("pageNavigation"),
             comp: {
-              logoUrl: '',
+              logoUrl: "",
               hidden: false,
               items: [
                 {
-                  label: 'Home',
+                  label: "Home",
                   hidden: false,
                   active: false,
                 },
                 {
-                  label: 'Services',
+                  label: "Services",
                   hidden: false,
                   active: false,
                   items: [
                     {
-                      label: 'Lowcode Platform',
+                      label: "Lowcode Platform",
                       hidden: false,
                       active: false,
                     },
                     {
-                      label: 'App Development',
+                      label: "App Development",
                       hidden: false,
                       active: false,
                     },
                   ],
                 },
                 {
-                  label: 'About',
+                  label: "About",
                   hidden: false,
                   active: false,
                 },
@@ -215,7 +203,7 @@ export function defaultPageLayoutData(
             },
           },
           layoutItem: {
-            i: '',
+            i: "",
             h: 6,
             w: 24,
             x: 0,
