210ddf7d2aa348dcb6c551f5de25e6953012878b
th37rose <thomasr37oss@gmail.com>
2024-05-18 16:51:21 +0000

    Make serversetting api response to json object(key-value pairs)

--- server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/config/ServerSettingController.java
+++ server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/config/ServerSettingController.java
@@ -1,12 +1,12 @@
 package org.lowcoder.api.config;
 
 import lombok.RequiredArgsConstructor;
-import org.lowcoder.api.framework.view.ResponseView;
 import org.lowcoder.domain.serversetting.model.ServerSetting;
-import org.lowcoder.domain.serversetting.service.ServerSettingRepository;
 import org.lowcoder.domain.serversetting.service.ServerSettingService;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,8 +14,8 @@ public class ServerSettingController implements ServerSettingEndpoints
 {
     private final ServerSettingService serverSettingService;
     @Override
-    public Flux<ServerSetting> getServerSettings() {
-        return serverSettingService.findAll();
+    public Mono<Map<String, String>> getServerSettings() {
+        return serverSettingService.getServerSettingsMap();
     }
 
 }
--- server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/config/ServerSettingEndpoints.java
+++ server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/config/ServerSettingEndpoints.java
@@ -1,14 +1,14 @@
 package org.lowcoder.api.config;
 
 import io.swagger.v3.oas.annotations.Operation;
-import org.lowcoder.api.framework.view.ResponseView;
-import org.lowcoder.domain.serversetting.model.ServerSetting;
 import org.lowcoder.infra.constant.NewUrl;
 import org.lowcoder.infra.constant.Url;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = {Url.SERVER_SETTING_URL, NewUrl.SERVER_SETTING_URL})
@@ -23,5 +23,5 @@ public interface ServerSettingEndpoints
 		    description = "Retrieve the list of server settings for Lowcoder."
 	)
     @GetMapping
-    Flux<ServerSetting> getServerSettings();
+	Mono<Map<String, String>> getServerSettings();
 }
