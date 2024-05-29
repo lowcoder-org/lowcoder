17ed9255182eee6865c5281c1419e5a4dbe88961
MeenamAfzal <meenamafzal1@gmail.com>
2024-05-30 00:17:45 +0500

    unused variable removed

--- client/packages/lowcoder/src/comps/generators/uiCompBuilder.tsx
+++ client/packages/lowcoder/src/comps/generators/uiCompBuilder.tsx
@@ -248,7 +248,6 @@ function UIView(props: {
 
   let defaultChildren = comp.children;
   const isNotContainer = defaultChildren.hasOwnProperty('style');
-  const restrictPaddingOnRotation = defaultChildren.hasOwnProperty('restrictPaddingOnRotation');
   let rotationVal = null
   let boxShadowVal = null;
   if (isNotContainer) {
@@ -266,7 +265,7 @@ function UIView(props: {
         margin: '0px',
         // padding:'0px',
         padding: (
-          rotationVal === null || rotationVal === undefined || restrictPaddingOnRotation
+          rotationVal === null || rotationVal === undefined
           ? '0px'
           : (
             boxShadowVal === null || boxShadowVal === undefined
