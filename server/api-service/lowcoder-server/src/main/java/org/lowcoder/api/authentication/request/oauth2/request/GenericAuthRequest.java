e0383d8b2c15fdc40b3e8fa8438ffba264599cf5
Thomas <thomasr37oss@gmail.com>
2024-06-07 14:13:04 -0400

    #763 Google relogin issue fix

--- server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/authentication/request/oauth2/request/GenericAuthRequest.java
+++ server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/authentication/request/oauth2/request/GenericAuthRequest.java
@@ -76,6 +76,7 @@ public class GenericAuthRequest  extends AbstractOauth2Request<Oauth2GenericAuth
 
     @Override
     protected Mono<AuthUser> getAuthUser(AuthToken authToken) {
+        if(!config.getUserInfoIntrospection()) return Mono.just(AuthUser.builder().build());
         return WebClientBuildHelper.builder()
                 .systemProxy()
                 .timeoutMs(HTTP_TIMEOUT)
