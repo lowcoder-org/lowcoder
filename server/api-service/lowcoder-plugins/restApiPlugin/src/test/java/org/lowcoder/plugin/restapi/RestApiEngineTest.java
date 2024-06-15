35d7ac437eb7472a3cd6e2266c21a2f376a355f5
FalkWolsky <fw@falkwolsky.com>
2024-06-15 12:38:12 +0200

    Small fix for Readme and test in restAPI Plugin

--- server/api-service/lowcoder-plugins/restApiPlugin/src/test/java/org/lowcoder/plugin/restapi/RestApiEngineTest.java
+++ server/api-service/lowcoder-plugins/restApiPlugin/src/test/java/org/lowcoder/plugin/restapi/RestApiEngineTest.java
@@ -180,7 +180,7 @@ public class RestApiEngineTest {
                     assertTrue(result.isSuccess());
                     assertNotNull(result.getData());
                     JsonNode url = ((ObjectNode) result.getData()).get("url");
-                    assertEquals("\"http://postman-echo.com/post?param=value+with+blank\"", url.toString());
+                    assertEquals("\"https://postman-echo.com/post?param=value+with+blank\"", url.toString());
                 })
                 .verifyComplete();
     }
