0bf3c8a6cd4478c3a28088c2e8dddee42ab218b0
FalkWolsky <fw@falkwolsky.com>
2024-06-15 16:46:54 +0200

    Update for Mobile Navigation Items Display

--- client/packages/lowcoder/src/comps/comps/layout/mobileTabLayout.tsx
+++ client/packages/lowcoder/src/comps/comps/layout/mobileTabLayout.tsx
@@ -31,6 +31,9 @@ import { AlignCenter } from "lowcoder-design";
 import { AlignLeft } from "lowcoder-design";
 import { AlignRight } from "lowcoder-design";
 import { defaultTheme } from "@lowcoder-ee/constants/themeConstants";
+import { AlignCenter } from "lowcoder-design";
+import { AlignLeft } from "lowcoder-design";
+import { AlignRight } from "lowcoder-design";
 
 const TabBar = React.lazy(() => import("antd-mobile/es/components/tab-bar"));
 const TabBarItem = React.lazy(() =>
