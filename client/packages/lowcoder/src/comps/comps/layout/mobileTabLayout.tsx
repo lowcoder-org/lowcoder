7fc918230d00057c14b154c1cc99740d1a471a59
RAHEEL <mraheeliftikhar1994@gmail.com>
2024-06-17 02:36:35 +0500

    remove duplicate dependecies

--- client/packages/lowcoder/src/comps/comps/layout/mobileTabLayout.tsx
+++ client/packages/lowcoder/src/comps/comps/layout/mobileTabLayout.tsx
@@ -33,9 +33,6 @@ import { AlignRight } from "lowcoder-design";
 import { LayoutActionComp } from "./layoutActionComp";
 import { clickEvent, eventHandlerControl } from "@lowcoder-ee/index.sdk";
 import { defaultTheme } from "@lowcoder-ee/constants/themeConstants";
-import { AlignCenter } from "lowcoder-design";
-import { AlignLeft } from "lowcoder-design";
-import { AlignRight } from "lowcoder-design";
 
 const TabBar = React.lazy(() => import("antd-mobile/es/components/tab-bar"));
 const TabBarItem = React.lazy(() =>
