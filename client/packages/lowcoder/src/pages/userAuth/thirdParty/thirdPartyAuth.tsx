a0a7c4e8135c97d54426fd51f13fb21258eeda0e
FalkWolsky <fw@falkwolsky.com>
2024-06-16 18:26:46 +0200

    Fixing Icon Display for OAuth Provider

--- client/packages/lowcoder/src/pages/userAuth/thirdParty/thirdPartyAuth.tsx
+++ client/packages/lowcoder/src/pages/userAuth/thirdParty/thirdPartyAuth.tsx
@@ -15,6 +15,7 @@ import { trans } from "i18n";
 import { geneAuthStateAndSaveParam, getAuthUrl, getRedirectUrl } from "pages/userAuth/authUtils";
 import { default as Divider } from "antd/es/divider";
 import { useRedirectUrl } from "util/hooks";
+import { MultiIconDisplay } from "../../../comps/comps/multiIconDisplay";
 
 const ThirdPartyLoginButtonWrapper = styled.div`
   button{
@@ -85,9 +86,12 @@ function ThirdPartyLoginButton(props: {
     ? `Sign up with ${label}`
     : `Sign in with ${label}`;
 
+  console.log('config', config);
+
   return (
     <StyledLoginButton buttonType="normal" onClick={onLoginClick}>
-      <LoginLogoStyle alt={config.name} src={config.logo} title={config.name} />
+      {config.icon && <MultiIconDisplay identifier={config.icon} width="20px" height="20px" style={{ marginRight: "20px", flexShrink: 0, color: "#000" }} />}
+      {!config.icon && <LoginLogoStyle alt={config.name} src={config.logo} title={config.name} />}
       <LoginLabelStyle className="auth-label">
         { buttonLabel }
       </LoginLabelStyle>
