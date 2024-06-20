29b3d04fbd7fd66036ff52209e30f3dcdfa0efa4
Thomas <thomasr37oss@gmail.com>
2024-06-20 16:17:44 -0400

    add JWT decoder merge user info from jwt and user endpoint

--- server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/authentication/request/oauth2/request/GenericAuthRequest.java
+++ server/api-service/lowcoder-server/src/main/java/org/lowcoder/api/authentication/request/oauth2/request/GenericAuthRequest.java
@@ -3,6 +3,7 @@ package org.lowcoder.api.authentication.request.oauth2.request;
 import org.lowcoder.api.authentication.request.AuthException;
 import org.lowcoder.api.authentication.request.oauth2.GenericOAuthProviderSource;
 import org.lowcoder.api.authentication.request.oauth2.OAuth2RequestContext;
+import org.lowcoder.api.authentication.util.JwtDecoderUtil;
 import org.lowcoder.domain.user.model.AuthToken;
 import org.lowcoder.domain.user.model.AuthUser;
 import org.lowcoder.sdk.auth.Oauth2GenericAuthConfig;
@@ -14,8 +15,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
-import static org.lowcoder.api.authentication.util.AuthenticationUtils.mapToAuthToken;
-import static org.lowcoder.api.authentication.util.AuthenticationUtils.mapToAuthUser;
+import static org.lowcoder.api.authentication.util.AuthenticationUtils.*;
 import static org.lowcoder.sdk.plugin.common.constant.Constants.HTTP_TIMEOUT;
 
 /**
@@ -75,7 +75,22 @@ public class GenericAuthRequest  extends AbstractOauth2Request<Oauth2GenericAuth
 
     @Override
     protected Mono<AuthUser> getAuthUser(AuthToken authToken) {
-        if(!Boolean.TRUE.equals(config.getUserInfoIntrospection())) return Mono.just(AuthUser.builder().build());
+        //parse the JWT token
+        String jwt = authToken.getJwt();
+        Map<String, Object> jwtMap = null;
+        if(jwt != null) {
+            try {
+                jwtMap = JwtDecoderUtil.decodeJwtPayload(jwt);
+            } catch (Exception ignored) {
+            }
+        }
+
+        if(!Boolean.TRUE.equals(config.getUserInfoIntrospection())) {
+            if(jwtMap == null) return Mono.error(new AuthException("No JWT token found"));
+            return Mono.just(mapToAuthUser(jwtMap, config.getSourceMappings()));
+        }
+
+        Map<String, Object> finalJwtMap = jwtMap;
         return WebClientBuildHelper.builder()
                 .systemProxy()
                 .timeoutMs(HTTP_TIMEOUT)
@@ -89,7 +104,8 @@ public class GenericAuthRequest  extends AbstractOauth2Request<Oauth2GenericAuth
                     if (map.containsKey("error") || map.containsKey("error_description")) {
                         return Mono.error(new AuthException(JsonUtils.toJson(map)));
                     }
-                    return Mono.just(mapToAuthUser(map, config.getSourceMappings()));
+                    AuthUser merged = mergeAuthUser(mapToAuthUser(finalJwtMap, config.getSourceMappings()), mapToAuthUser(map, config.getSourceMappings()));
+                    return Mono.just(merged);
                 });
     }
 }
