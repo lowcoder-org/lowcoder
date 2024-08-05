f4a35920b6838b32a86ba9d930478242ed01c156
Thomasr <techninjas514@gmail.com>
2024-08-05 12:33:08 -0400

    Add firebase spec version

--- server/node-service/src/plugins/firebase/index.ts
+++ server/node-service/src/plugins/firebase/index.ts
@@ -3,12 +3,18 @@ import dataSourceConfig, { DataSourceDataType } from "./dataSourceConfig";
 import queryConfig, { ActionDataType } from "./queryConfig";
 import { runFirebasePlugin } from "./run";
 
+const specs = {
+  "v1.0": queryConfig
+}
+
 const firebasePlugin: DataSourcePlugin<ActionDataType, DataSourceDataType> = {
   id: "firebase",
   icon: "firebase.svg",
   name: "Firebase",
   category: "api",
-  queryConfig,
+  queryConfig: async (data) => {
+    return specs[data.specVersion as keyof typeof specs]
+  },
   dataSourceConfig,
   run: function (actionData, dataSourceConfig): Promise<any> {
     return runFirebasePlugin(actionData, dataSourceConfig);
