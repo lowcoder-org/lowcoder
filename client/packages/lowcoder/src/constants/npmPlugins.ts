8847dae017624a829627c22a23e635872cbe2b6c
FalkWolsky <fw@falkwolsky.com>
2024-08-23 12:42:47 +0200

    Adapting URL ROUTE Names and changing NPM Repository Paths

--- client/packages/lowcoder/src/constants/npmPlugins.ts
+++ client/packages/lowcoder/src/constants/npmPlugins.ts
@@ -1,5 +1,3 @@
-import { sdkConfig } from "./sdkConfig";
-
-const baseUrl = sdkConfig.baseURL || LOWCODER_NODE_SERVICE_URL || "";
-export const NPM_REGISTRY_URL = `${baseUrl}/node-service/api/npm/registry`;
-export const NPM_PLUGIN_ASSETS_BASE_URL = `${baseUrl}/node-service/api/npm/package`;
+export const SERVER_HOST = `${REACT_APP_API_SERVICE_URL ?? ""}`;
+export const NPM_REGISTRY_URL = `${SERVER_HOST}/api/npm/registry`;
+export const NPM_PLUGIN_ASSETS_BASE_URL = `${SERVER_HOST}/api/npm/package`;
