ac9cb311ae0923679a2d37feae4cdbd477aa9c53
MeenamAfzal <meenamafzal1@gmail.com>
2024-09-24 00:07:48 +0500

    ColumnLayout: Added Main Content Scrollbar

--- client/packages/lowcoder/src/comps/comps/pageLayoutComp/pageLayout.tsx
+++ client/packages/lowcoder/src/comps/comps/pageLayoutComp/pageLayout.tsx
@@ -157,7 +157,7 @@ export function PageLayout(props: LayoutProps & { siderCollapsed: boolean; setSi
           }}
         >
       <Wrapper $style={style} $animationStyle={animationStyle}>
-        <Layout style={{padding: "0px", overflow: container.mainScrollbars ? "scroll" : "inherit"}} hasSider={showSider && !container.innerSider}>
+        <Layout style={{padding: "0px", overflow: container.mainScrollbars ? "scroll" : "visible"}} hasSider={showSider && !container.innerSider}>
           {showSider && !container.innerSider && !container.siderRight && (
             <><BackgroundColorContext.Provider value={siderStyle?.siderBackground}>
               <Sider 
