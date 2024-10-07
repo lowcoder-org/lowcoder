711337bed73acb41628ade59580026f5d63efe1a
Imiss-U1025 <imissu1025@gmail.com>
2024-10-07 18:14:50 -0400

    optimize view

--- client/packages/lowcoder-design/src/util/language.ts
+++
@@ -1,2 +0,0 @@
-export const isEditor = () => window.location.href.includes("edit") ? "edit" : window.location.href.includes("view") ? "view" : "admin"
-export const getLanguage = () => localStorage.getItem('lowcoder_uiLanguage')
