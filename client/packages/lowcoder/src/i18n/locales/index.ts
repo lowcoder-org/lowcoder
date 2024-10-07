42513e0a5177476c79a9c0e14dc5c592215afe32
Imiss-U1025 <imissu1025@gmail.com>
2024-10-07 11:39:58 -0400

    split admin, editor, viewer of language basically

--- client/packages/lowcoder/src/i18n/locales/index.ts
+++ client/packages/lowcoder/src/i18n/locales/index.ts
@@ -1,14 +1,14 @@
-// file examples: en, enGB, zh, zhHK
-// fallback example: current locale is zh-HK, fallback order is zhHK => zh => en
-export * from "./de";
-export * from "./en";
-export * from "./it";
-export * from "./pt";
-export * from "./es";
-export * from "./zh";
-export * from "./ru";
+import { isEditor } from "util/editor"
+export const en = async ():Promise<object> => await import(isEditor() === "edit" ? "./enEditor" : isEditor() === "view" ? "./enViewer" : "./en").then(module => module.en);
+export const de = async ():Promise<object> => await import(isEditor() === "edit" ? "./deEditor" : isEditor() === "view" ? "./deViewer" : "./de").then(module => module.de);
+export const it = async ():Promise<object> => await import(isEditor() === "edit" ? "./itEditor" : isEditor() === "view" ? "./itViewer" : "./it").then(module => module.it);
+export const pt = async ():Promise<object> => await import(isEditor() === "edit" ? "./ptEditor" : isEditor() === "view" ? "./ptViewer" : "./pt").then(module => module.pt);
+export const es = async ():Promise<object> => await import(isEditor() === "edit" ? "./esEditor" : isEditor() === "view" ? "./esViewer" : "./es").then(module => module.es);
+export const zh = async ():Promise<object> => await import(isEditor() === "edit" ? "./zhEditor" : isEditor() === "view" ? "./zhViewer" : "./zh").then(module => module.zh);
+export const ru = async ():Promise<object> => await import(isEditor() === "edit" ? "./ruEditor" : isEditor() === "view" ? "./ruViewer" : "./ru").then(module => module.ru);
 
-export * from "./enObj";
-export * from "./deObj";
-export * from "./itObj";
-export * from "./zhObj";
+
+export const enObj = async ():Promise<object> => await import("./enObj").then(module => (module.enObj));
+export const deObj = async ():Promise<object | undefined> => await import("./deObj").then(module => (module.deObj));
+export const itObj = async ():Promise<object> => await import("./itObj").then(module => (module.itObj));
+export const zhObj = async ():Promise<object | undefined> => await import("./zhObj").then(module => (module.zhObj));
