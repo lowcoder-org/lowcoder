f52fd50680705776381b842fbda52d0fc32e65d4
Imiss-U1025 <imissu1025@gmail.com>
2024-11-05 13:55:15 -0500

    Changed viewMode of processing languag

--- client/packages/lowcoder/src/i18n/locales/index.ts
+++ client/packages/lowcoder/src/i18n/locales/index.ts
@@ -1,11 +1,11 @@
-import { viewMode } from "util/editor"
-export const en = async ():Promise<object> => await import(viewMode() === "edit" ? "./enEditor" : viewMode() === "view" ? "./enViewer" : "./en").then(module => module.en);
-export const de = async ():Promise<object> => await import(viewMode() === "edit" ? "./deEditor" : viewMode() === "view" ? "./deViewer" : "./de").then(module => module.de);
-export const it = async ():Promise<object> => await import(viewMode() === "edit" ? "./itEditor" : viewMode() === "view" ? "./itViewer" : "./it").then(module => module.it);
-export const pt = async ():Promise<object> => await import(viewMode() === "edit" ? "./ptEditor" : viewMode() === "view" ? "./ptViewer" : "./pt").then(module => module.pt);
-export const es = async ():Promise<object> => await import(viewMode() === "edit" ? "./esEditor" : viewMode() === "view" ? "./esViewer" : "./es").then(module => module.es);
-export const zh = async ():Promise<object> => await import(viewMode() === "edit" ? "./zhEditor" : viewMode() === "view" ? "./zhViewer" : "./zh").then(module => module.zh);
-export const ru = async ():Promise<object> => await import(viewMode() === "edit" ? "./ruEditor" : viewMode() === "view" ? "./ruViewer" : "./ru").then(module => module.ru);
+import { viewModeTriple } from "util/editor"
+export const en = async ():Promise<object> => await import(viewModeTriple() === "edit" ? "./enEditor" : viewModeTriple() === "view" ? "./enViewer" : "./en").then(module => module.en);
+export const de = async ():Promise<object> => await import(viewModeTriple() === "edit" ? "./deEditor" : viewModeTriple() === "view" ? "./deViewer" : "./de").then(module => module.de);
+export const it = async ():Promise<object> => await import(viewModeTriple() === "edit" ? "./itEditor" : viewModeTriple() === "view" ? "./itViewer" : "./it").then(module => module.it);
+export const pt = async ():Promise<object> => await import(viewModeTriple() === "edit" ? "./ptEditor" : viewModeTriple() === "view" ? "./ptViewer" : "./pt").then(module => module.pt);
+export const es = async ():Promise<object> => await import(viewModeTriple() === "edit" ? "./esEditor" : viewModeTriple() === "view" ? "./esViewer" : "./es").then(module => module.es);
+export const zh = async ():Promise<object> => await import(viewModeTriple() === "edit" ? "./zhEditor" : viewModeTriple() === "view" ? "./zhViewer" : "./zh").then(module => module.zh);
+export const ru = async ():Promise<object> => await import(viewModeTriple() === "edit" ? "./ruEditor" : viewModeTriple() === "view" ? "./ruViewer" : "./ru").then(module => module.ru);
 
 
 export const enObj = async ():Promise<object | undefined> => await import("./enObj").then(module => (module.enObj));
--- client/packages/lowcoder/src/util/language.ts
+++ client/packages/lowcoder/src/util/language.ts
@@ -1,19 +1,19 @@
-import {viewMode} from "@lowcoder-ee/util/editor";
+import {viewModeTriple} from "@lowcoder-ee/util/editor";
 
 interface LangJsonType {
     [key: string]: any;
 }
 
 export const setLanguageJson = (lang : string, langObj: any) => {
-    localStorage.setItem(`lowcoder_language_json_${lang}_${viewMode()}`, JSON.stringify(langObj));
+    localStorage.setItem(`lowcoder_language_json_${lang}_${viewModeTriple()}`, JSON.stringify(langObj));
 }
 
 export const setLanguageObjJson = (lang : string, langObj: any) => {
-    localStorage.setItem(`lowcoder_language_obj_json_${lang}_${viewMode()}`, JSON.stringify(langObj));
+    localStorage.setItem(`lowcoder_language_obj_json_${lang}_${viewModeTriple()}`, JSON.stringify(langObj));
 }
 
 export const getLanguageJson = (lang: string): LangJsonType | null => {
-    const item = localStorage.getItem(`lowcoder_language_json_${lang}_${viewMode()}`);
+    const item = localStorage.getItem(`lowcoder_language_json_${lang}_${viewModeTriple()}`);
 
     // Check if the item is null
     if (item === null) {
@@ -27,7 +27,7 @@ export const getLanguageJson = (lang: string): LangJsonType | null => {
 };
 
 export const getLanguageObjJson = (lang: string): LangJsonType | null => {
-    const item = localStorage.getItem(`lowcoder_language_obj_json_${lang}_${viewMode()}`);
+    const item = localStorage.getItem(`lowcoder_language_obj_json_${lang}_${viewModeTriple()}`);
 
     // Check if the item is null
     if (item === null) {
