fb3aecf2189afae9956490753330231db35f1834
RAHEEL <mraheeliftikhar1994@gmail.com>
2024-11-07 20:56:01 +0500

    fix lowcoder-comps base url in embedded apps

--- client/packages/lowcoder/src/constants/npmPlugins.ts
+++ client/packages/lowcoder/src/constants/npmPlugins.ts
@@ -1,7 +1,7 @@
 // export const SERVER_HOST = `${REACT_APP_NODE_SERVICE_URL ?? ""}`;
 // export const NPM_REGISTRY_URL = `${SERVER_HOST}/node-service/api/npm/registry`;
 // export const NPM_PLUGIN_ASSETS_BASE_URL = `${SERVER_HOST}/node-service/api/npm/package`;
-
+export const ASSETS_BASE_URL = `api/npm/package`;
 export const SERVER_HOST = `${REACT_APP_API_SERVICE_URL ?? ""}`;
 export const NPM_REGISTRY_URL = `${SERVER_HOST}/api/npm/registry`;
 export const NPM_PLUGIN_ASSETS_BASE_URL = `${SERVER_HOST}/api/npm/package`;
