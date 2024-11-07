539544c1e843c7386d8c17075d858a567c967fc0
Imiss-U1025 <imissu1025@gmail.com>
2024-11-07 03:14:55 -0500

    Added build:view in package.json, changed viewModeTriple.

--- client/packages/lowcoder/src/util/editor.ts
+++ client/packages/lowcoder/src/util/editor.ts
@@ -1,5 +1,5 @@
 export const viewMode = () =>  REACT_APP_VIEW_MODE;
-export const viewModeTriple = () => window.location.href.includes("view") ? "view" : window.location.href.includes("edit") ? "edit" : "admin";
+export const viewModeTriple = () => viewMode() === "view" ? "view" : window.location.href.includes("edit") ? "edit" : "admin";
 export const getLanguage = (): string => {
     return localStorage.getItem('lowcoder_uiLanguage') || 'en';
 }
