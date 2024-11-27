fa580fb49508c22eb76041d9a4cf9869033f090f
Imiss-U1025 <imissu1025@gmail.com>
2024-11-27 01:26:18 -0500

    Added utilities for pagination.

--- client/packages/lowcoder/src/util/pagination/type.ts
+++ client/packages/lowcoder/src/util/pagination/type.ts
@@ -12,3 +12,25 @@ export const ApplicationPaginationType: ApplicationType = {
     6: "MOBILETABLAYOUT",
     7: "NAVIGATION",
 };
+
+export interface fetchAppRequestType {
+    pageNum?: number;
+    pageSize?: number;
+    name?: string;
+    applicationType?: number;
+}
+
+export interface fetchFolderRequestType {
+    pageNum?: number;
+    pageSize?: number;
+    name?: string;
+    applicationType?: string;
+}
+
+export interface GenericApiPaginationResponse<T> {
+    total: number;
+    success: boolean;
+    code: number;
+    message: string;
+    data: T;
+}
