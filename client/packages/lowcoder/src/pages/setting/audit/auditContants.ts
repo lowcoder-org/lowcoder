32752dd41d31e6e88e7004efb1e7597d3b72c56c
RAHEEL <mraheeliftikhar1994@gmail.com>
2025-01-18 15:05:32 +0500

    added echarts-for-react package and created chart for audit logs

---
+++ client/packages/lowcoder/src/pages/setting/audit/auditContants.ts
@@ -0,0 +1,7 @@
+import ReactECharts from 'echarts-for-react';
+import { styled } from 'styled-components';
+
+export const StyledReactECharts = styled(ReactECharts)`
+  width: 100%;
+  height: 400px;
+`;
--- client/packages/lowcoder/src/pages/setting/audit/index.tsx
+++ client/packages/lowcoder/src/pages/setting/audit/index.tsx
@@ -1,3 +1,19 @@
+import ReactECharts from 'echarts-for-react';
+import { DetailContainer, DetailContent, Header } from '../theme/styledComponents';
+import { HeaderBack } from '../permission/styledComponents';
+import { AreaTimeChart } from './AreaTimeChart';
+
 export default function Audit() {
-  return null;
+  return (
+    <DetailContainer>
+      <Header>
+        <HeaderBack>
+          <span>{"Audit Logs"}</span>
+        </HeaderBack>
+      </Header>
+      <DetailContent>
+        <AreaTimeChart />
+      </DetailContent>
+    </DetailContainer>
+  )
 }
