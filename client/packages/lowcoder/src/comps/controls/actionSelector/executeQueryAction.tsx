c8b9c18dbf826b70748aaa50f50883802f76583c
RAHEEL <mraheeliftikhar1994@gmail.com>
2025-03-21 02:18:31 +0500

    fixed infinite loop for js queries

--- client/packages/lowcoder/src/comps/controls/actionSelector/executeQueryAction.tsx
+++ client/packages/lowcoder/src/comps/controls/actionSelector/executeQueryAction.tsx
@@ -56,6 +56,9 @@ const ExecuteQueryPropertyView = ({
   }, [placement]);
 
   const getVariableOptions = useCallback((editorState?: EditorState) => {
+    const queryVariables = comp.children.queryVariables.getView();
+    if (!queryVariables.length) return null;
+
     return comp.children.queryVariables.propertyView({
       label: trans("eventHandler.queryVariables"),
       layout: "vertical",
