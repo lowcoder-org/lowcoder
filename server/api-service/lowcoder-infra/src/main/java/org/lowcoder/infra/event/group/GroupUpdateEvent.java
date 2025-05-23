62a72b4122dd60e491645569032474fdd98b1437
Thomasr <techninjas514@thecoderagency.com>
2025-05-23 14:04:32 -0400

    Added before/after detail of group member event

--- server/api-service/lowcoder-infra/src/main/java/org/lowcoder/infra/event/group/GroupUpdateEvent.java
+++ server/api-service/lowcoder-infra/src/main/java/org/lowcoder/infra/event/group/GroupUpdateEvent.java
@@ -5,6 +5,9 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class GroupUpdateEvent extends BaseGroupEvent {
 
+    private final String groupName;
+    private final String oldGroupName;
+
     @Override
     public EventType getEventType() {
         return EventType.GROUP_UPDATE;
