b19ebe7baab8ad7b4a310ff237f30b996cceade5
FalkWolsky <fw@falkwolsky.com>
2025-05-24 23:47:54 +0200

    Updating API-Service to drop legacy lowcoderAPIPlugin and publish new lowcoder-comps version

--- server/api-service/lowcoder-domain/src/main/java/org/lowcoder/domain/plugin/DatasourceMetaInfoConstants.java
+++ server/api-service/lowcoder-domain/src/main/java/org/lowcoder/domain/plugin/DatasourceMetaInfoConstants.java
@@ -4,8 +4,9 @@ public final class DatasourceMetaInfoConstants {
 
     public static final String REST_API = "restApi";
     public static final String GRAPHQL_API = "graphql";
+    public static final String JS_CODE_EXECUTION = "javaScriptCode";
     public static final String MYSQL = "mysql";
-    public static final String LOWCODER_API = "lowcoderApi";
+    // public static final String LOWCODER_API = "lowcoderApi";
 
     private DatasourceMetaInfoConstants() {
     }
