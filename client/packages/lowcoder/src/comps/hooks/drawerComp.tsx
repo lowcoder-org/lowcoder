e8bc8815a9b2e1252bfc3df3b2d4e7a414159b68
Faran Javed <faran1997@outlook.com>
2025-10-09 19:55:47 +0500

    [Fix]: #1290 add zindex property for stacking issue

--- client/packages/lowcoder/src/comps/hooks/drawerComp.tsx
+++ client/packages/lowcoder/src/comps/hooks/drawerComp.tsx
@@ -4,7 +4,7 @@ import { ContainerCompBuilder } from "comps/comps/containerBase/containerCompBui
 import { gridItemCompToGridItems, InnerGrid } from "comps/comps/containerComp/containerView";
 import { AutoHeightControl } from "comps/controls/autoHeightControl";
 import { BoolControl } from "comps/controls/boolControl";
-import { StringControl } from "comps/controls/codeControl";
+import { StringControl, NumberControl } from "comps/controls/codeControl";
 import { booleanExposingStateControl } from "comps/controls/codeStateControl";
 import { PositionControl, LeftRightControl, HorizontalAlignmentControl } from "comps/controls/dropdownControl";
 import { eventHandlerControl } from "comps/controls/eventHandlerControl";
@@ -122,6 +122,7 @@ const childrenMap = {
   showMask: withDefault(BoolControl, true),
   toggleClose:withDefault(BoolControl,true),
   escapeClosable: withDefault(BoolControl, true),
+  zIndex: withDefault(NumberControl, Layers.drawer),
 };
 
 type ChildrenType = NewChildren<RecordConstructorToComp<typeof childrenMap>> & {
@@ -167,6 +168,9 @@ const DrawerPropertyView = React.memo((props: {
       })}
       {props.children.escapeClosable.propertyView({
         label: trans("prop.escapeClose"),
+      })}
+      {props.children.zIndex.propertyView({
+        label: trans("prop.zIndex"),
       })}
     </Section>
     <Section name={sectionNames.interaction}>{props.children.onEvent.getPropertyView()}</Section>
@@ -251,7 +255,7 @@ const DrawerView = React.memo((
           height={!props.autoHeight ? transToPxSize(props.height || DEFAULT_SIZE) : ""}
           onClose={onClose}
           afterOpenChange={afterOpenChange}
-          zIndex={Layers.drawer}
+          zIndex={props.zIndex}
           maskClosable={props.maskClosable}
           mask={true}
           className={clsx(`app-${appID}`, props.className)}
