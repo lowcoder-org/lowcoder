9a900cc80885f48c63b46dc406ab683332761ed5
Ludo Mikula <ludovit.mikula@mikori.sk>
2025-10-20 19:49:57 +0200

    fix: normalize max request size for nginx

--- deploy/docker/frontend/01-update-nginx-conf.sh
+++ deploy/docker/frontend/01-update-nginx-conf.sh
@@ -18,12 +18,16 @@ else
    ln -s /etc/nginx/nginx-http.conf /etc/nginx/nginx.conf
 fi;
 
-sed -i "s@__LOWCODER_MAX_REQUEST_SIZE__@${LOWCODER_MAX_REQUEST_SIZE:=20m}@" /etc/nginx/nginx.conf
+# Normalize max. request size for usage with nginx
+MAX_REQUEST_SIZE=$(echo "${LOWCODER_MAX_REQUEST_SIZE:=20m}" | perl -pe 's/^([ \t]*)(?<number>\d+(\.\d+)?)([ \t]*)(?<unit>[kKmMgGtT]{1})?([bB \t]*)$/"$+{number}" . lc($+{unit})/e')
+
+
+sed -i "s@__LOWCODER_MAX_REQUEST_SIZE__@${MAX_REQUEST_SIZE}@" /etc/nginx/nginx.conf
 sed -i "s@__LOWCODER_MAX_QUERY_TIMEOUT__@${LOWCODER_MAX_QUERY_TIMEOUT:=120}@" /etc/nginx/server.conf
 sed -i "s@__LOWCODER_API_SERVICE_URL__@${LOWCODER_API_SERVICE_URL:=http://localhost:8080}@" /etc/nginx/server.conf
 sed -i "s@__LOWCODER_NODE_SERVICE_URL__@${LOWCODER_NODE_SERVICE_URL:=http://localhost:6060}@" /etc/nginx/server.conf
 
 echo "nginx config updated with:"
-echo "    Lowcoder max upload size: ${LOWCODER_MAX_REQUEST_SIZE:=20m}"
+echo "    Lowcoder max upload size: ${MAX_REQUEST_SIZE:=20m}"
 echo "    Lowcoder api service URL: ${LOWCODER_API_SERVICE_URL:=http://localhost:8080}"
 echo "   Lowcoder node service URL: ${LOWCODER_NODE_SERVICE_URL:=http://localhost:6060}"
